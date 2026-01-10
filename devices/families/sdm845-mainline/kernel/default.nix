0b5ca726393da9902b7a03ce743bf8e6d6883bf5
Petr Hodina <petr.hodina@protonmail.com>
2026-01-10 15:23:32 +0000

    sdm845-mainline: Build linux-next 6.19 (next-20260106)
    Signed-off-by: Petr Hodina <petr.hodina@protonmail.com>

--- devices/families/sdm845-mainline/kernel/default.nix
+++ devices/families/sdm845-mainline/kernel/default.nix
@@ -1,28 +1,89 @@
-{ mobile-nixos
-, fetchFromGitLab
-, fetchpatch
-, ...
+{ mobile-nixos,
+  fetchFromGitea,
+  fetchpatch,
+  stdenv,
+  bc,
+  bison,
+  flex,
+  gcc,
+  gnumake,
+  perl,
+  python3,
+  zstd, # To compress kernel modules
+  features ? [],
+  ...
 }:
 
-mobile-nixos.kernel-builder {
-  version = "6.4.0";
-  configfile = ./config.aarch64;
-
-  src = fetchFromGitLab {
-    owner = "sdm845-mainline";
+let
+  kernelSrc = fetchFromGitea {
+    domain = "codeberg.org";
+    owner = "sdm845";
     repo = "linux";
-    rev = "sdm845-6.4-r1";
-    hash = "sha256-XUYv8tOk0vsG11w8UtBKizlBZ03cbQ2QRGyZEK0ECGU=";
+    rev = "dc7b19cffd9ef96b28da5441061ff26dce1025a6";
+    hash = "sha256-5DJ/G7I193TVnVn08dA0UYtzSf6g/Us6/ep8DCaFAdQ=";
   };
 
-  patches = [
-    # ASoC: codecs: tas2559: Fix build
-    (fetchpatch {
-      url = "https://github.com/samueldr/linux/commit/d1b59edd94153ac153043fb038ccc4e6c1384009.patch";
-      sha256 = "sha256-zu1m+WNHPoXv3VnbW16R9SwKQzMYnwYEUdp35kUSKoE=";
-    })
-  ];
+  configfile = stdenv.mkDerivation {
+    name = "sdm845-kernel-config";
+    src = kernelSrc;
 
-  isModular = false;
+    nativeBuildInputs = [
+      gnumake
+      gcc
+      bc
+      bison
+      flex
+      perl
+      python3
+      zstd
+    ];
+
+    buildPhase = ''
+            export ARCH=arm64
+            export KCONFIG_CONFIG=$PWD/.config
+
+            # Start with defconfig
+            make defconfig
+
+            # Merge sdm845.config fragment if it exists
+            ./scripts/kconfig/merge_config.sh -m .config \
+              arch/arm64/configs/sdm845.config \
+              ${./defconfig}
+
+            # Add essential NixOS required kernel options
+            cat >>.config <${./defconfig}
+
+            # Run olddefconfig to resolve dependencies
+            make olddefconfig
+
+            cp .config config
+    '';
+
+    installPhase = ''
+      cp config $out
+    '';
+  };
+in
+
+mobile-nixos.kernel-builder {
+  version = "6.19.0-rc4-next-20260106-sdm845";
+  configfile = configfile;
+  src = kernelSrc;
+
+  patches = [ ];
+
+  nativeBuildInputs = [ python3 zstd ];
+
+  makeFlags = [ "dtbs" ];
+
+  # Don't use zinstall, it expects EFI boot files which ARM64 doesn't generate
+  installTargets = [ ];
+
+  postInstall = ''
+    echo ":: Installing Image.gz kernel"
+    cp -v "$buildRoot/arch/arm64/boot/Image.gz" "$out/Image.gz"
+  '';
+
+  isModular = true;
   isCompressed = "gz";
 }
