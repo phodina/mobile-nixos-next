91a13b3dc1a7f64c8fe2a84e561f366d0b1b6b83
Petr Hodina <petr.hodina@protonmail.com>
2026-01-12 20:57:17 +0000

    phosh: smaller build

--- examples/phosh/phosh.nix
+++ examples/phosh/phosh.nix
@@ -17,17 +17,36 @@
     group = "users";
   };
 
-  programs.calls.enable = true;
+  programs.calls.enable = lib.mkDefault true;
 
   environment.systemPackages = with pkgs; [
     # Disabled since it uses `olm` which was marked insecure.
     #chatty              # IM and SMS
-    epiphany            # Web browser
+    # epiphany            # Web browser
     gnome-console       # Terminal
-    megapixels          # Camera
+    # libcamera         # Camera support (disabled to reduce size)
+    # megapixels        # Camera
   ];
 
-  hardware.sensor.iio.enable = true;
+  hardware.sensor.iio.enable = lib.mkDefault true;
+
+  # Size optimization options
+  documentation = {
+    enable = lib.mkDefault false;
+    doc.enable = lib.mkDefault false;
+    man.enable = lib.mkDefault false;
+    nixos.enable = lib.mkDefault false;
+    info.enable = lib.mkDefault false;
+  };
+
+  # Reduce closure size
+  environment.noXlibs = lib.mkDefault false; # Can't disable X libs for Phosh
+
+  # Minimize locales - keep only English
+  i18n.supportedLocales = lib.mkDefault [ "en_US.UTF-8/UTF-8" ];
+
+  # Disable unnecessary firmware
+  hardware.enableAllFirmware = lib.mkDefault false;
 
   assertions = [
     { assertion = options.services.xserver.desktopManager.phosh.user.isDefined;
