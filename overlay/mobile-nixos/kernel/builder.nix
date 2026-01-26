0fc1b3c6a0d80a77c59b6a117ada8f820a606a62
Petr Hodina <petr.hodina@protonmail.com>
2026-01-26 11:10:29 +0000

    kernel/builder: Remove the 'pie' flag as it's enabled in compiler

--- overlay/mobile-nixos/kernel/builder.nix
+++ overlay/mobile-nixos/kernel/builder.nix
@@ -540,7 +540,7 @@ stdenv.mkDerivation ((inputArgs // {
     + maybeString postInstall
   ;
 
-  hardeningDisable = [ "bindnow" "format" "fortify" "stackprotector" "pic" "pie" ];
+  hardeningDisable = [ "bindnow" "format" "fortify" "stackprotector" "pic" ];
 
   makeFlags = [
     "O=$(buildRoot)"
