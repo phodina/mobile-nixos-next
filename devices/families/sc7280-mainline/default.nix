5b34544f905bcf493c1e9f4f3017b5bf3e5b489f
Petr Hodina <petr.hodina@protonmail.com>
2026-02-06 18:36:56 +0000

    sc7280-mainline: Enable android sparse image
    Signed-off-by: Petr Hodina <petr.hodina@protonmail.com>

--- devices/families/sc7280-mainline/default.nix
+++ devices/families/sc7280-mainline/default.nix
@@ -30,6 +30,7 @@
   ];
 
   mobile.system.type = "android";
+  mobile.system.android.useSparseImage = true;
   mobile.system.android = {
     # Assumed all SC7280 devices use A/B
     ab_partitions = lib.mkDefault true;
