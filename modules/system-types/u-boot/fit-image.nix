0d2aec6e2f61b400e1b914c1eb89fa79bbfd33f3
Petr Hodina <petr.hodina@protonmail.com>
2026-02-06 19:15:22 +0000

    modules/u-boot: Add fit-image

---
+++ modules/system-types/u-boot/fit-image.nix
@@ -0,0 +1,101 @@
+{ lib
+, pkgs
+, name
+, kernel
+, initrd
+, dtbs ? []
+}:
+
+let
+  inherit (pkgs) buildPackages;
+  inherit (lib) concatMapStringsSep;
+  
+  fdtEntries = lib.imap0 (idx: dtb: ''
+    fdt-${toString idx} {
+      description = "Device Tree ${toString idx}";
+      data = /incbin/("${dtb}");
+      type = "flat_dt";
+      arch = "arm64";
+      compression = "none";
+      hash-1 {
+        algo = "sha256";
+      };
+    };
+  '') dtbs;
+  
+  configEntries = lib.imap0 (idx: dtb: ''
+    config-${toString idx} {
+      description = "Configuration ${toString idx}";
+      kernel = "kernel";
+      ramdisk = "ramdisk";
+      fdt = "fdt-${toString idx}";
+    };
+  '') dtbs;
+  
+  defaultConfig = if dtbs != [] then "config-0" else null;
+  
+  itsContent = ''
+    /dts-v1/;
+    
+    / {
+      description = "Mobile NixOS FIT Image";
+      #address-cells = <1>;
+      
+      images {
+        kernel {
+          description = "Linux Kernel";
+          data = /incbin/("${kernel}");
+          type = "kernel";
+          arch = "arm64";
+          os = "linux";
+          compression = "none";
+          load = <0x01080000>;
+          entry = <0x01080000>;
+          hash-1 {
+            algo = "sha256";
+          };
+        };
+        
+        ramdisk {
+          description = "Initrd";
+          data = /incbin/("${initrd}");
+          type = "ramdisk";
+          arch = "arm64";
+          os = "linux";
+          compression = "none";
+          hash-1 {
+            algo = "sha256";
+          };
+        };
+        
+        ${concatMapStringsSep "\n    " (x: x) fdtEntries}
+      };
+      
+      ${lib.optionalString (dtbs != []) ''
+      configurations {
+        ${lib.optionalString (defaultConfig != null) ''
+        default = "${defaultConfig}";
+        ''}
+        
+        ${concatMapStringsSep "\n    " (x: x) configEntries}
+      };
+      ''}
+    };
+  '';
+  
+in
+pkgs.runCommand "${name}-fit" {
+  nativeBuildInputs = with buildPackages; [
+    ubootTools
+    dtc
+  ];
+  passAsFile = [ "itsContent" ];
+  inherit itsContent;
+} ''
+  cp "$itsContentPath" image.its
+  
+  mkimage -f image.its $out
+  
+  echo "FIT image created: $out"
+  ls -lh $out
+''
